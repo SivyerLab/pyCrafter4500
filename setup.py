d7a6d2d3e0cb3ddab97b2dac01726ad489946bff
awctomlinson <mexander@gmail.com>
2019-10-16 15:11:00 -0700

    update setup

--- setup.py
+++ setup.py
@@ -1,17 +1,24 @@
-from distutils.core import setup
+from setuptools import setup
+from pathlib import Path
+
+README = Path('README.md').read_text()
 
 setup(
-    name = 'pycrafter4500',
-    packages = ['pycrafter4500'], # this must be the same as the name above
-    version = '0.5',
-    description = 'A python interface to communicate over USB with the TI Lightcrafter 4500',
-    author = 'Alexander Tomlinson',
-    author_email = 'tomlinsa@ohsu.eud',
-    url = 'https://github.com/SivyerLab/pyCrafter4500',
-    download_url = 'https://github.com/SivyerLab/pyCrafter4500/archive/0.5.tar.gz',
-    keywords = 'lightcrafter 4500 dlpc 350 projector texas',
-    install_requires=[
-        'pyusb'
-    ],
-    license='MIT',
+    name='pycrafter4500',
+    version='0.6',
+    packages=['pycrafter4500'],
+    install_requires=['pyusb'],
+
+    # pypi metadata
+    author='Alexander Tomlinson',
+    author_email='tomlinsa@ohsu.edu',
+    description='A python interface to communicate over USB with the TI Lightcrafter 4500',
+    long_description=README,
+    long_description_content_type="text/markdown",
+    keywords='lightcrafter 4500 dlpc 350 projector texas',
+    url='https://pycrafter4500.readthedocs.io/en/latest/',
+    project_urls={
+        "Source Code": 'https://github.com/SivyerLab/pyCrafter4500',
+    },
+    license='GPL',
 )
